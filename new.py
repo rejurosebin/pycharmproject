c60231255016896a1815f3fd1ef2be763b938c38
rejurosebin <rejuhelan@gmail.com>
2021-11-21 21:02:01 -0800

    first commit

---
+++ new.py
@@ -0,0 +1,69 @@
+##x=15
+##y=4
+##print('x+y=',x+y)
+##print('x-y',x-y)
+##print('x*y',x*y)
+##print('x/y',x/y)
+##print('x%y',x%y)
+##print('x**y',x**y)
+##print('x//y',x//y)
+##print('x>y',x>y)
+##print('x<y',x<y)
+##print('x==y',x==y)
+##print('x!=y',x!=y)
+##print('x>=y',x>=y)
+##print('x<=y',x<=y)
+##
+##A=21
+##B=10
+##C=A+B
+##print("line1= The value of C is",C)
+##C-=5
+##print("Line2= The value of C is",C)
+##C*=5
+##print("Line3= The value of C is",C)
+##C/=5
+##print("Line4= The value of C is",C)
+##C%=5
+##print("Line5= The value of C is",C)
+##C//=5
+##print("Line6= The value of C is",C)
+##C**=5
+##print("Line7= The value of C is",C)
+##C>>=5
+##print("Line8= The value of C is",C)
+##C<<=5
+##print("Line9= The value of C is",C)
+
+##x=10
+##y=4
+##print('x&y=',x&y)
+##print('x|y=',x|y)
+##print('~x=',~x)
+##print('x^y=',x^y)
+##print('x>>y=',x>>y)
+##print('x<<y=',x<<y)
+##print('x>>2=',x>>2)
+##print('x<<2',x<<2)
+
+##x= True
+##y= False
+##print('x and y is',x and y)
+##print('x or y is',x or y)
+##print('not x is',not x)
+
+##x='Hello world'
+##print('H'in x)
+##print('hello'not in x)
+
+X1=5
+Y1=5
+X2='Hello'
+Y2='Hello'
+print(X1 is not Y1)
+print(X2 is Y2)
+
+a=10
+print(a)
+
+
